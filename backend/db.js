b15d7d9eee3e8c1af68901b931f76521bbeb62c6
Enayat <enayatansari33@gmail.com>
2025-09-03 12:33:23 +0530

    update some file

--- backend/db.js
+++ backend/db.js
@@ -4,10 +4,10 @@ const mysql = require("mysql2");
 const db_pass = process.env.DB_PASSWORD;
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",       
-  password: db_pass, 
-  database: "school_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,       
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME
 });
 
 db.connect((err) => {
